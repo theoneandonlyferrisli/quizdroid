652d400d069cb29f0b79a667fd8b3e773685b7f2
Yuefeng Li <radek@uw.edu>
2015-05-19 15:52:56 -0700

    project initialized

---
+++ app/src/main/java/lifecounter/radek/washington/edu/quizdroidparti/JsonTopicRepo.java
@@ -0,0 +1,11 @@
+package lifecounter.radek.washington.edu.quizdroidparti;
+
+/**
+ * Created by apple on 5/12/15.
+ *
+ * A repository that reads topics from the json file under assets directory.
+ */
+
+public class JsonTopicRepo {
+
+}
